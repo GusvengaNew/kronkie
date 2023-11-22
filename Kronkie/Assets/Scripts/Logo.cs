cae78efe5797a4398130f480b76955173105cdbf
GusvengaNew <gusvengaofficial@gmail.com>
2023-11-22 18:03:22 +0100

    nutty nutyyyy
    lol

---
+++ Kronkie/Assets/Scripts/Logo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Logo : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(WaitAndChange());
+    }
+
+    IEnumerator WaitAndChange()
+    {
+        yield return new WaitForSeconds(8);
+    }
+
+	public void ChangeScene(string sceneName)
+	{
+		SceneManager.LoadScene (name);
+	}
+}
