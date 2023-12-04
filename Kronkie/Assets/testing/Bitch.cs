0c4d0a5b4c1779db1a3bcdb837447b1eee9722de
GusvengaNew <gusvengaofficial@gmail.com>
2023-12-04 21:05:20 +0100

    added thingies

---
+++ Kronkie/Assets/testing/Bitch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bitch : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log ("Fuck butt");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log ("vore my butt");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log ("funny haha spam");
+    }
+}
---
+++ Kronkie/Assets/testing/TestScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    int a = 69;
+    float b = 3;
+    string myDick = "Epic";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log (myDick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
