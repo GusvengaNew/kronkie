fa64d7b62c1199e3df8af55b59652b59171a7709
GusvengaNew <gusvengaofficial@gmail.com>
2023-12-06 18:45:41 +0100

    06.12.2023

---
+++ Kronkie/Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    void Update()
+    {
+        // Example: Check for a specific condition (e.g., press a key) to destroy the object
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            // Destroy the GameObject this script is attached to
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Kronkie/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public float timeToSwitchScene = 5f; // Adjust this to the desired time before switching scenes
+    public string sceneToLoad; // Variable to store the name of the scene to switch to
+
+    private float timer = 0f;
+
+    void Update()
+    {
+        // Update the timer
+        timer += Time.deltaTime;
+
+        // Check if the timer has reached the desired time
+        if (timer >= timeToSwitchScene)
+        {
+            // Reset the timer
+            timer = 0f;
+
+            // Call a method to switch scenes
+            SwitchScene();
+        }
+    }
+
+    void SwitchScene()
+    {
+        // Check if the sceneToLoad variable is not empty
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            // Load the specified scene
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogError("SceneToLoad is not set in the inspector!");
+        }
+    }
+}
