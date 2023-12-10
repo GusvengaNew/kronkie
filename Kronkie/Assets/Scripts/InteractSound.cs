a094477af7de0378e6ae90006ffff7ff85d14a9a
GusvengaNew <gusvengaofficial@gmail.com>
2023-12-10 19:03:22 +0100

    10.12.2023

---
+++ Kronkie/Assets/Scripts/InteractSound.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class InteractSound : MonoBehaviour
+{
+    public AudioClip soundClip;
+    public KeyCode interactKey = KeyCode.E;
+    public float activationRadius = 5f;
+
+    private AudioSource audioSource;
+    private bool canInteract = true;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            Debug.LogError("AudioSource component not found on the GameObject.");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(interactKey) && canInteract && IsPlayerNearby())
+        {
+            PlaySound();
+            StartCoroutine(DelayInteraction());
+        }
+    }
+
+    void PlaySound()
+    {
+        if (audioSource != null && soundClip != null)
+        {
+            audioSource.PlayOneShot(soundClip);
+        }
+    }
+
+    bool IsPlayerNearby()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+            return distance <= activationRadius;
+        }
+        return false;
+    }
+
+    System.Collections.IEnumerator DelayInteraction()
+    {
+        canInteract = false;
+        yield return new WaitForSeconds(1f);
+        canInteract = true;
+    }
+}
